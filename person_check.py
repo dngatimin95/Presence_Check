05262ae8a3c1ed282d652e084fbec6b898d7a0f4
dngatimin <dngatimin95@gmail.com>
2020-09-24 15:15:44 -0400

    Update person_check.py

--- person_check.py
+++ person_check.py
@@ -52,7 +52,7 @@ while True:
 
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 69, 255), 2)
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 69, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_COMPLEX
+        font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (0, 0, 0), 1)
         present.add(name)
 
